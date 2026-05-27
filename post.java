806c9ddfa3540bfc8b860f4564aa678520ac5f94
Satyam ojha <osatyam27@gmail.com>
2026-05-27 22:29:51 +0530

    Enhance Post class with imagePath and comments
    Added imagePath and timestamp fields, and implemented comments functionality.

--- post.java
+++ post.java
@@ -1,26 +1,49 @@
 package socialapp;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Post {
-    public String content;
-    public Set<String> likedUsers;
 
-    public Post(String content) {
+    public String content;
+    public String imagePath;
+
+    public Set<String> likedUsers;
+    public List<String> comments;
+
+    public String timestamp;
+
+    public Post(String content, String imagePath) {
+
         this.content = content;
-        this.likedUsers = new HashSet<>();
+        this.imagePath = imagePath;
+
+        likedUsers = new HashSet<>();
+        comments = new ArrayList<>();
+
+        timestamp = new Date().toString();
     }
 
+    // Like System
     public void toggleLike(String username) {
+
         if (likedUsers.contains(username)) {
+
             likedUsers.remove(username);
+
         } else {
+
             likedUsers.add(username);
         }
     }
 
     public int getLikeCount() {
+
         return likedUsers.size();
+    }
+
+    // Comment System
+    public void addComment(String comment) {
+
+        comments.add(comment);
     }
 }
